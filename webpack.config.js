3256c3c182d19cb95cabeadf4ae7380ab1d6dc6b
chunghe <chunghe_fang@htc.com>
2015-08-25 21:41:01 +0800

    move source code to /src, generated code to /dist, use babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,23 @@
 var webpack = require('webpack');
 var path  = require('path');
 var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
-var data = require('./data')
+var data = require('./src/data')
 
 module.exports = {
-  entry: './entry.js',
+  entry: './src/entry.js',
 
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
     libraryTarget: 'umd'
   },
 
   module: {
-    loaders: [
-      { test: /\.jsx$/, loader: 'jsx-loader' }
-    ]
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: path.join(__dirname, 'src')
+    }]
   },
 
   plugins: [
